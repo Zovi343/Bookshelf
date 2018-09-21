bc4be46c7b03330a1d5b2e6caec31358e5487359
Zovi <39067421+VaelinAlSorna@users.noreply.github.com>
2018-09-21 17:53:59 +0200

    When there are no results after search message is showed to the user

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -16,15 +16,22 @@ export class SearchResults extends React.Component {
                 {
                     !!this.props.apiErrText
                         ? <p  className="api-error">{this.props.apiErrText}</p>
-                        : this.props.searchValue !== this.props.searchValueBefore
-                            ? <div className="loader"> 
-                                <img className="loader__image" src="/images/loader.gif" />
-                                </div>
-                            :<ol>
-                                {
-                                    this.props.searchResults.map((book) => <SearchResultItem getBookClick={this.getBookClick} key={book.id} book={book} /> )
-                                }
-                            </ol> 
+                        : !this.props.searchValue
+                            ? <div className="message">
+                                <h3 className="message__item">Search for something!</h3>
+                              </div>
+                            : this.props.searchValue !== this.props.searchValueBefore
+                                ? <div className="loader"> 
+                                    <img className="loader__image" src="/images/loader.gif" />
+                                    </div>
+                                : !!this.props.searchResults.map((book) => book).length
+                                    ?<ol> 
+                                        { this.props.searchResults.map((book) => <SearchResultItem getBookClick={this.getBookClick} key={book.id} book={book} />) } 
+                                    </ol> 
+                                    :<div className="no-results">
+                                        <h2 className="no-results__item">No Results</h2>
+                                    </div>
+                                 
                 }
             </div>
         ); 
